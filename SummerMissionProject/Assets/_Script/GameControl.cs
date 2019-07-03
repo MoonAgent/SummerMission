303a04c3c94c7ffd10e73ac50719a42d409d643b
Darks.lee <darks.lee@coconut.island.studio.com>
2019-07-03 12:24:41 +0800

    Revert "Revert "summerMission工程文件""
    This reverts commit 7824fa3a8a8fbc649d6fe849d467239bac968ca5.

---
+++ SummerMissionProject/Assets/_Script/GameControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public GameObject m_cubeObj;
+    public float m_heightVal;
+    public Slider m_slider;
+    public Text m_text;
+
+    void Start()
+    {
+        ChangeHeight();
+    }
+
+    private void ChangeHeight(float t_heigt = 1f)
+    {
+        m_heightVal = t_heigt;
+        m_cubeObj.transform.localScale = new Vector3(1f, m_heightVal, 1f);
+    }
+
+    void Update()
+    {
+        m_text.text = m_slider.value.ToString();
+        ChangeHeight(m_slider.value);
+    }
+}
