80b33f3a8613f2d6fef1a6b6a9aecff3dd51911a
getoutoflom <lomjldsnl@gmail.com>
2019-07-04 00:15:17 +0800

    02-任浩
    介绍了打大A和一个由自己设计的扑克游戏：兵临城下。
    根据demo制作一个简单的球体变化。

---
+++ SummerMissionProject/Assets/New Folder/New.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class New : MonoBehaviour
+{
+    public GameObject m_sphereObj;
+    public float m_radiusVal;
+    public Slider m_slider;
+    public Text m_text;
+
+    void Start()
+    {
+        ChangeRadius();
+    }
+
+    private void ChangeRadius(float t_radius = 1f)
+    {
+        m_radiusVal = t_radius;
+        m_sphereObj.transform.localScale = new Vector3(m_radiusVal, m_radiusVal, m_radiusVal);
+    }
+
+    void Update()
+    {
+        m_text.text = m_slider.value.ToString();
+        ChangeRadius(m_slider.value);
+    }
+}
