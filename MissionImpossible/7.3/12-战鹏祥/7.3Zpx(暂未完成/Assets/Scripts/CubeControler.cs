5bb19fb634cea841749403097dfff04958f032b2
HappyAuratus <45818560+HappyAuratus@users.noreply.github.com>
2019-07-04 06:43:33 +0800

    SummerMission

--- MissionImpossible/7.3/12-战鹏祥/7.3Zpx(暂未完成/Assets/Scripts/CubeControler.cs
+++ MissionImpossible/7.3/12-战鹏祥/7.3Zpx(暂未完成/Assets/Scripts/CubeControler.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CubeControler : MonoBehaviour
 {
-    public GameObject cube;
+    public GameObject m_cube1;
+    public GameObject m_cube2;
+    public Text m_text;
 
+    public Slider m_slider;
+    public Slider m_slider2;
     // Start is called before the first frame update
     void Start()
     {
-        cube.GetComponent<MeshRenderer>().material.color = Color.blue;
+        ChangeCubeColor();
     }
-
+    void ChangeCubeColor()
+    {
+        m_cube1.GetComponent<MeshRenderer>().material.color = new Vector4(m_slider.value, m_slider.value, m_slider.value, 1);
+        if(m_slider2.value>=0&& m_slider2.value<=0.25)
+        {
+            m_cube2.GetComponent<MeshRenderer>().material.color = new Vector4(0, 0, m_slider2.value*4, 1);
+            m_text.text = "Black<->Blue";
+        }
+        if(m_slider2.value > 0.25 && m_slider2.value <= 0.5)
+        {
+            m_cube2.GetComponent<MeshRenderer>().material.color = new Vector4(0,(m_slider2.value - 0.25f), 1-(m_slider2.value-0.25f) * 4, 1);
+            m_text.text = "Blue<->Green";
+        }
+        if (m_slider2.value > 0.5 && m_slider2.value <= 0.75)
+        {
+            m_cube2.GetComponent<MeshRenderer>().material.color = new Vector4((m_slider2.value - 0.5f)*4, 1, 0, 1);
+            m_text.text = "Green<->Yellow";
+        }
+        if (m_slider2.value > 0.75 && m_slider2.value <= 1)
+        {
+            m_cube2.GetComponent<MeshRenderer>().material.color = new Vector4(1, 1- (m_slider2.value - 0.75f)*4, 0, 1);
+            m_text.text = "Yellow<->Red";
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeCubeColor();
     }
 }
