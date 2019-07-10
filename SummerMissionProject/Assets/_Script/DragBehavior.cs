86635444eb91c378c106480c82f5157a8bfa0970
Darks.lee <darks.lee@coconut.island.studio.com>
2019-07-10 11:28:51 +0800

    710案例

---
+++ SummerMissionProject/Assets/_Script/DragBehavior.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragBehavior : MonoBehaviour,IDragHandler,IEndDragHandler
+{
+    public GameObject triggerObj;
+    public PlaceName belongPlace;
+    public MainControl710 mainControl;
+
+    void Start()
+    {
+        triggerObj = null;
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //print(eventData.position);
+        Vector3 v3 = Camera.main.ScreenToWorldPoint(eventData.position);
+        this.transform.position = new Vector3(v3.x, v3.y, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "land")
+        {
+            //print("land");
+            triggerObj = collision.gameObject;
+        }
+
+        if (collision.tag == "boat")
+        {
+            //print("boat");
+            triggerObj = collision.gameObject;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (triggerObj == null)
+            return;
+
+        if (triggerObj.tag == "boat")
+        {
+            SetSeat(PlaceName.Boat);
+            //belongPlace = PlaceName.Boat;
+            
+        }
+        else if (triggerObj.tag == "land")
+        {
+            //belongPlace = PlaceName.LLand;
+            SetSeat(PlaceName.LLand);
+        }
+        else if (triggerObj.tag == "land2")
+        {
+            ///belongPlace = PlaceName.RLand;
+            SetSeat(PlaceName.RLand);
+        }
+    }
+
+    private void SetSeat(PlaceName _PN)
+    {
+        if (triggerObj.transform.childCount == 0)
+        {
+            belongPlace = _PN;
+            this.transform.SetParent(triggerObj.transform);
+            this.transform.localPosition = Vector2.zero;
+        }
+        else if (triggerObj.transform.childCount == 1)
+        {
+            GameObject _obj = triggerObj.transform.GetChild(0).gameObject;
+            Transform _tp = this.transform.parent;
+            PlaceName _pn = this.belongPlace;
+
+            transform.parent = _obj.transform.parent.transform;
+            _obj.transform.parent = _tp;
+            belongPlace = _obj.GetComponent<DragBehavior>().belongPlace;
+            _obj.GetComponent<DragBehavior>().belongPlace = _pn;
+
+            this.transform.localPosition = Vector2.zero;
+            _obj.transform.localPosition = Vector2.zero;
+            mainControl.IntoSomePlace(_obj.GetComponent<DragBehavior>());
+        }
+        mainControl.IntoSomePlace(this);
+    }
+}
---
+++ SummerMissionProject/Assets/_Script/MainControl710.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControl710 : MonoBehaviour
+{
+    public List<DragBehavior> CharLis;
+    public List<Transform> SLandLis1;
+    public List<Transform> SLandLis2;
+    public List<Transform> SBoatLis;
+
+
+    public List<GameObject> LandLis1 = new List<GameObject>();
+    public List<GameObject> LandLis2 = new List<GameObject>();
+    public List<GameObject> BoatLis = new List<GameObject>();
+
+
+
+    void Start()
+    {
+        InitGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 v3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D c2d = Physics2D.OverlapPoint(v3);
+            if (c2d != null && c2d.name == "Button")
+            {
+                KaiChuan();
+            }
+        }
+    }
+
+    public void InitGame()
+    {
+        for (int i = 0; i < CharLis.Count; i++)
+        {
+            CharLis[i].transform.SetParent(SLandLis1[i]);
+            CharLis[i].transform.localPosition = Vector2.zero;
+            CharLis[i].belongPlace = PlaceName.LLand;
+            CharLis[i].mainControl = this;
+            LandLis1.Add(CharLis[i].gameObject);
+        }
+    }
+
+    public void IntoSomePlace(DragBehavior _db)
+    {
+        LandLis1.Remove(_db.gameObject);
+        LandLis2.Remove(_db.gameObject);
+        BoatLis.Remove(_db.gameObject);
+
+        if (_db.belongPlace == PlaceName.LLand)
+        {
+            LandLis1.Add(_db.gameObject);
+        }
+        else if (_db.belongPlace == PlaceName.RLand)
+        {
+            LandLis2.Add(_db.gameObject);
+        }
+        else if (_db.belongPlace == PlaceName.Boat)
+        {
+            BoatLis.Add(_db.gameObject);
+        }
+    }
+
+    public void KaiChuan()
+    {
+        print("开船");
+    }
+}
+
+public enum PlaceName
+{
+    LLand,
+    RLand,
+    Boat
+}
