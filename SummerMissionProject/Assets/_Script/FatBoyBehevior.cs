eac52e57335cc2736188d50fa462d59a5b3e6c32
Darks.lee <darks.lee@coconut.island.studio.com>
2019-07-14 10:38:26 +0800

    动画案例

---
+++ SummerMissionProject/Assets/_Script/FatBoyBehevior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatBoyBehevior : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer sprite;
+    public float speed;
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        sprite = this.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        animator.SetBool("isWalk", Mathf.Abs(move) > 0);
+        //animator.SetFloat("isWalkf", Mathf.Abs(move));
+        if (Mathf.Abs(move) > 0)
+            sprite.flipX = !(move > 0);
+        transform.Translate(move, 0f, 0f);
+    }
+}
